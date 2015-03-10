ee6eec980a2bd963f01482fb5d2f33c22cd00250
Panu Leppäniemi <me@panuleppaniemi.com>
2015-03-10 07:33:42 +0200

    Testing, popup for easier debugging

--- src/VisualAnalysisAction.java
+++ src/VisualAnalysisAction.java
@@ -1,12 +1,40 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.ui.awt.RelativePoint;
 
 public class VisualAnalysisAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        System.out.println(e.getPlace());
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        int caretOffset = editor.getCaretModel().getOffset();
+
+        PsiFile currentFile = e.getData(PlatformDataKeys.PSI_FILE);
+        PsiElement currentElement = currentFile.findElementAt(caretOffset);
+        
+        String message = currentElement.getNode().toString();
+        
+        show(e, message);
     }
-    
-    public void update(AnActionEvent e) {
-        System.out.println(e.getPlace());
+
+    private void show(AnActionEvent e, String message) {
+        StatusBar statusBar = WindowManager.getInstance().getStatusBar(DataKeys.PROJECT.getData(e.getDataContext()));
+
+        JBPopupFactory.getInstance()
+            .createHtmlTextBalloonBuilder(message, MessageType.INFO, null)
+            .setFadeoutTime(15000)
+            .createBalloon()
+            .show(
+                RelativePoint.getCenterOf(statusBar.getComponent()),
+                Balloon.Position.atRight
+            );
     }
 }
